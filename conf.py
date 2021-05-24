00a0d0ae459c39461de1888dd1e4d8f11f04298a
Tyson L. Swetnam <tyson-swetnam@users.noreply.github.com>
2021-05-24 14:16:59 -0700

    Update conf.py

--- conf.py
+++ conf.py
@@ -10,9 +10,9 @@ sys.path.insert(0, os.path.dirname(__file__))
 
 from misc.cyverse_sphinx_conf import *  # noqa
 
-project = 'YOUR QUICKSTART NAME HERE as topic_quick_start'
-copyright = '2020, CyVerse'
-author = 'your_name'
+project = 'change-password-quickstart'
+copyright = '2021, CyVerse'
+author = 'Michael Culshaw-Maurer'
 version = '2.0'
 release = '2.0'
 
